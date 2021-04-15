7140a3b32a66c46c9479b15717fc18ead9230a77
NerisaK <nerisakalosin@seznam.cz>
2021-04-15 19:39:39 +0200

    setting wardrobe colors added

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="bg-neutral">
     <app-header />
-    <div class="container bg-olive my-5">
+    <div class="bg-olive m-5">
+      <show-colors />
     </div>
     <app-footer />
   </div>
@@ -10,19 +11,25 @@
 <script>
 import AppHeader from './components/TheHeader.vue'
 import AppFooter from './components/TheFooter.vue'
+import ShowColors from './components/ShowColors'
 
 export default {
   name: 'App',
   components: {
     AppHeader,
-    AppFooter
+    AppFooter,
+    ShowColors
+  },
+  data(){
+    return{
+      colors: [],
+    }
   }
 }
 </script>
 
 <style>
   .bg-orange{background-color: #ffc168;}
-  .bg-green{background-color: #c0de9e}
   .bg-neutral{background-color: #e6e9e2}
   .bg-grey{background-color:#3C403D}
 </style>
---
+++ src/components/ShowColors.vue
@@ -0,0 +1,147 @@
+<template>
+    <div>
+        <keep-alive>
+            <div v-if="showColors">
+                <p v-if="colorsNotSet"><strong>
+                    Začni výběrem hlavních barev pro svůj šatník!
+                </strong></p>
+                <h3 class="mb-4" v-if="!colorsNotSet">Tvoje barvy</h3>
+                <div class="palette-three">
+                    <div
+                        class="color"
+                        :style="{ backgroundColor: colors[0] }"
+                    ></div>
+                    <div
+                        class="color"
+                        :style="{ backgroundColor: colors[1] }"
+                    ></div>
+                    <div
+                        class="color"
+                        :style="{ backgroundColor: colors[2] }"
+                    ></div>
+                </div>
+                <div class="palette-two">
+                    <div
+                        class="color"
+                        :style="{ backgroundColor: colors[3] }"
+                    ></div>
+                    <div
+                        class="color"
+                        :style="{ backgroundColor: colors[4] }"
+                    ></div>
+                </div>
+                <button
+                    class="btn btn-success my-3 ml-2"
+                    @click="showColors = !showColors, showSettings = !showSettings"
+                    >
+                    Vybrat nové barvy
+                </button>
+            </div>
+        </keep-alive>
+        <form v-if="showSettings">
+            <h5>Hlavní barvy (akcenty):</h5>
+            <div>
+                <input
+                    type="color"
+                    name="color-picker0"
+                    id="color-picker0"
+                    v-model.lazy="newColors[0]"
+                />
+                <input
+                    type="color"
+                    name="color-picker1"
+                    id="color-picker1"
+                    v-model.lazy="newColors[1]"
+                />
+                <input
+                    type="color"
+                    name="color-picker2"
+                    id="color-picker2"
+                    v-model.lazy="newColors[2]"
+                />
+            </div>
+            <h5>Neutrální barvy (doplňkové):</h5>
+            <div>
+                <input
+                    type="color"
+                    name="color-picker3"
+                    id="color-picker3"
+                    v-model.lazy="newColors[3]"
+                />
+                 <input
+                    type="color"
+                    name="color-picker4"
+                    id="color-picker4"
+                    v-model.lazy="newColors[4]"
+                />
+            </div>
+            <p class="info">Pokud si svůj výběr později rozmyslíš, můžeš se sem vrátit, ale pamatuj na to, že barva se nezmění jen ve tvé paletě, ale i u všech věcí, které máš tou barvou označené!</p> 
+            <button
+                class="btn btn-success"
+                @click.prevent="changeColors(), showColors = !showColors, showSettings = !showSettings"
+                >
+                Uložit změny
+            </button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            colorsNotSet: false,
+            showColors: true,
+            showSettings: false,
+            colors: [
+                "#C57C7C",
+                "#768A3D",
+                "#7A1A1A",
+                "#4b3520",
+                "#ffffff"
+            ],
+            newColors: [
+                "#C57C7C",
+                "#768A3D",
+                "#7A1A1A",
+                "#4b3520",
+                "#ffffff"
+            ],
+        }
+    },
+    methods: {
+        changeColors(){
+            this.colors = this.newColors.slice();
+        }
+    }    
+}
+</script>
+
+<style scoped>
+    .palette-three, .palette-two, form div {
+        display: flex;
+    }
+
+    .palette-three .color{
+        width: 60px;
+        height: 60px;
+        margin: 5px;
+    }
+
+    .palette-two .color{
+        width: 95px;
+        height: 60px;
+        margin: 5px;
+    }
+
+    .color{border: 2px solid #3C403D}
+
+    input{
+        width: 3rem;
+        height: 3rem;
+        margin-right: 10px;
+        margin-bottom: 30px
+    }  
+
+    .info{width: 50%}  
+</style>
